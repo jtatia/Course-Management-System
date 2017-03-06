e7f75a8f4e4fc1e01489bfcbc910132266aa8ad3
the_shashwat <shashwattiwariknp@gmail.com>
2017-03-07 00:12:00 +0530

    Added the Course class

---
+++ src/main/course/course/Course.java
@@ -0,0 +1,62 @@
+package main.course.course;
+
+public class Course {
+	
+	private int serialNo;   	// primary key, not Null , auto-increment
+	
+	private String courseId; 	// primary key, not Null  
+	
+	private String courseName;	// full name of course e.g. Algorithms
+
+	private String courseInfo;	// String type course Info (longtext/blob in the database) 
+	
+	public Course()
+	{
+		
+	}
+	
+	public Course(int serialNo,String courseId,String courseInfo)
+	{
+		this();
+		this.serialNo=serialNo;
+		this.courseInfo=courseInfo;
+		this.courseId=courseId;
+	}
+	
+	public int getSerialNo() {
+		return serialNo;
+	}
+
+	public void setSerialNo(int serialNo) {
+		this.serialNo = serialNo;
+	}
+
+	public String getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	public String getCourseInfo() {
+		return courseInfo;
+	}
+
+	public void setCourseInfo(String courseInfo) {
+		this.courseInfo = courseInfo;
+	}
+	
+	public String toString()
+	{
+		return courseId+" - "+courseName; 
+	}
+}
